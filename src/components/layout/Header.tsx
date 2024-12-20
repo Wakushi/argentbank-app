dc2e29b9377f6be7500969dd3f2621e0bea88c89
Maxime ELIAZORD <mzozord@gmail.com>
2024-12-20 09:15:41 +0100

    Updated header design and fixed nav

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
 import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import { AppDispatch } from "../../store"
 import { isLogged, logout } from "../../features/user/authSlice"
+import { getUserInfo } from "../../selectors"
+import { useSelector } from "react-redux"
 
 export default function Header() {
   const dispatch = useDispatch<AppDispatch>()
   const navigate = useNavigate()
+  const user = useSelector(getUserInfo)
 
   function handleSignOut() {
     dispatch(logout())
@@ -19,27 +22,28 @@ export default function Header() {
   return (
     <header>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="/">
+        <NavLink className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
             src="/images/argentBankLogo.png"
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </NavLink>
         <div>
-          {isLogged() ? (
-            <>
-              <a href="/profile" className="main-nav-item">
-                Profile
-              </a>
+          {isLogged() && !!user ? (
+            <div className="logged-nav">
+              <NavLink to="/profile" className="main-nav-item">
+                <i className="fa fa-user-circle"></i> {user.firstName}
+              </NavLink>
               <button className="main-nav-item" onClick={handleSignOut}>
-                <i className="fa fa-user-circle"></i>Sign Out
+                <i className="fa fa-sign-out" aria-hidden="true"></i>
+                Sign Out
               </button>
-            </>
+            </div>
           ) : (
             <button className="main-nav-item" onClick={handleSignIn}>
-              <i className="fa fa-user-circle"></i>Sign In
+              <i className="fa fa-sign-in"></i>Sign In
             </button>
           )}
         </div>
